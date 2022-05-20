c22414337d989733ae56aac452ff43b22020923e
sunquake <kaskyr@gmail.com>
2022-05-20 11:11:09 +0600

    v1

--- cache.go
+++ cache.go
@@ -3,21 +3,37 @@ package cache
 import "time"
 
 type Cache struct {
+	m map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{map[time.Now().toString()]""}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	//if c.time < time.Now() {
+		return c.m[key], true
+		//}
+	//return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.m[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var s []string
+	for _, v := range c.m {
+		s = append(s,v)
+	}
+	return s
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	var s []string
+	for _, v := range c.m {
+		//if
+		s = append(s,v)
+	}
+	return s
 }
