a9feeeb7a42fb5763339c4b9b1e6b1cd739b0de7
sunquake <kaskyr@gmail.com>
2022-05-20 12:03:45 +0600

    v2

--- cache.go
+++ cache.go
@@ -7,33 +7,28 @@ type Cache struct {
 }
 
 func NewCache() Cache {
-	return Cache{map[time.Now().toString()]time.Now()}
+	return Cache{map[string]time.Time{time.Now().String():time.Now()}}
 }
 
 func (c Cache) Get(key string) (string, bool) {
 	//if c.time < time.Now() {
-		return c.m[key].toString(), true
+		return c.m[key].String(), true
 		//}
 	//return "", false
 }
 
 func (c Cache) Put(key, value string) {
-	c.m[key] = time.Parse(time.RFC3339, value)
+	c.m[key],_ = time.Parse(time.RFC3339, value)
 }
 
 func (c Cache) Keys() []string {
 	var s []string
 	for _, v := range c.m {
-		s = append(s,v.toString())
+		s = append(s,v.String())
 	}
 	return s
 }
 
 func (c Cache) PutTill(key, value string, deadline time.Time) {
-	var s []string
-	for _, v := range c.m {
-		//if time.Parse(key)
-		s = append(s,v.toString())
-	}
-	return s
+	
 }
