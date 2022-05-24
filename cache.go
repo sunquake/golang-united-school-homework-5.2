4360a1867e5f2bf9b4d5c35ac5bfcf086748c672
sunquake <kaskyr@gmail.com>
2022-05-24 17:23:18 +0600

    struct fix

--- cache.go
+++ cache.go
@@ -2,27 +2,30 @@ package cache
 
 import "time"
 
+type ctime struct {
+	val string
+	tim time.Time
+}
+
 type Cache struct {
-	data map[string]string
-	dead map[string]time.Time
+	data map[string]ctime
 }
 
 func NewCache() Cache {
-	return Cache{data:map[string]string{}, dead:map[string]time.Time{}}
+	return Cache{data:map[string]ctime{}}
 }
 
 func (c Cache) Get(key string) (string, bool) {
-	if c.dead[key].After(time.Now()) {
-		return c.data[key], true
+	if c.data[key].tim.After(time.Now()) {
+		return c.data[key].val, true
 		}
 	delete(c.data, key)
-	delete(c.dead, key)
 	return "", false
 }
 
 func (c Cache) Put(key, value string) {
-	c.data[key] = value
-	c.dead[key] = time.Now().AddDate(100,0,0)
+	c.data[key].val = value
+	c.dead[key].tim = time.Now().AddDate(100,0,0)
 }
 
 func (c Cache) Keys() []string {
@@ -34,6 +37,6 @@ func (c Cache) Keys() []string {
 }
 
 func (c Cache) PutTill(key, value string, deadline time.Time) {
-	c.data[key] = value
-	c.dead[key] = deadline
+	c.data[key].val = value
+	c.dead[key].tim = deadline
 }
